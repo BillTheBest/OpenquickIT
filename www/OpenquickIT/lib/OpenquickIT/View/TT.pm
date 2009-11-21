5137db0197aee1587fc05d8945211188e54a2db2
Iñigo Tejedor Arrondo <poisonbit@gmail.com>
2009-11-21 15:19:23 +0100

    script/openquickit_create.pl view TT TT

---
+++ www/OpenquickIT/lib/OpenquickIT/View/TT.pm
@@ -0,0 +1,33 @@
+package OpenquickIT::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+OpenquickIT::View::TT - TT View for OpenquickIT
+
+=head1 DESCRIPTION
+
+TT View for OpenquickIT.
+
+=head1 SEE ALSO
+
+L<OpenquickIT>
+
+=head1 AUTHOR
+
+Iñigo Tejedor Arrondo,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
